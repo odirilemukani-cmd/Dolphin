5cb994c147dabcc941b8bb1f47f426cd4b2cd599
odirilemukani-cmd <odirilemukani@gmail.com>
2026-09-08 05:26:42 +0200

    Create status.py

---
+++ api/status.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        response = {
+            "bot": "Dolphin",
+            "status": "online",
+            "mode": "paper",
+            "symbol": "XAUUSD",
+            "timeframe": "M5"
+        }
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(
+            json.dumps(response).encode("utf-8")
+        )
