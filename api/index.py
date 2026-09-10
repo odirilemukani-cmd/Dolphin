489a963b76c646997828fd1982e01cefeafe6f26
odirilemukani-cmd <odirilemukani@gmail.com>
2026-09-10 22:32:53 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,17 +1,35 @@
 from http.server import BaseHTTPRequestHandler
 import json
 
+from _viper import (
+    BOT_NAME,
+    json_safe_state
+)
+
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+
         response = {
-            "bot": "Dolphin",
-            "status": "online"
+            "bot": BOT_NAME,
+            "status": "online",
+            "message": "Viper API is online",
+            "state": json_safe_state()
         }
 
         self.send_response(200)
-        self.send_header("Content-Type", "application/json")
+
+        self.send_header(
+            "Content-Type",
+            "application/json"
+        )
+
+        self.send_header(
+            "Cache-Control",
+            "no-store"
+        )
+
         self.end_headers()
 
         self.wfile.write(
